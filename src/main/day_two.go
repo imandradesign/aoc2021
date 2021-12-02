565283e7822a4548a573e5515e2a2feeb377ca5e
Imandra McKenzie <imckenzie@squarespace.com>
2021-12-02 14:39:55 -0800

    Reorganize files. Day 2 Part 1 doesn't work yet.

--- src/main/day_two.go
+++ src/main/day_two.go
@@ -1,8 +1,7 @@
 package main
 
 func calculatePosition(list []string, words []string, nums []int) (mult int) {
-	horizPosition := 0
-	depth := 0
+	var horizPosition, depth, aim int
 
 	for i := range list {
 		currentWord := words[i]
@@ -10,18 +9,25 @@ func calculatePosition(list []string, words []string, nums []int) (mult int) {
 
 		if currentWord == "forward" {
 			horizPosition += currentNum
+			depth += (aim * currentNum)
 		}
 
 		if currentWord == "up" {
 			depth -= currentNum
+			aim -= currentNum
 		}
 
 		if currentWord == "down" {
 			depth += currentNum
+			aim += currentNum
 		}
 	}
 
 	mult = horizPosition * depth
+
+	log.Infof("Final horizontal position: %v", horizPosition)
+	log.Infof("Final depth: %v", depth)
+	log.Infof("Final aim: %v", aim)
 
 	return mult
 }
--- src/main/main.go
+++ src/main/main.go
@@ -12,7 +12,7 @@ var (
 
 func main() {
 	// Input from Day 1
-	inputOne, err := readInputOne("input1.txt")
+	inputOne, err := readInputOne("inputs/input1.txt")
 	if err != nil {
 		log.WithError(err).Error("Error with Day 1 input.")
 	}
@@ -22,12 +22,12 @@ func main() {
 	log.Infof("Day 1 - Depth count: %v", depthCount)
 
 	// Input from Day 2
-	inputFullTwo, inputWordsTwo, inputNumsTwo, err := readInputTwo("input2.txt")
+	inputFullTwo, inputWordsTwo, inputNumsTwo, err := readInputTwo("inputs/input2.txt")
 	if err != nil {
 		log.WithError(err).Error("Error with Day 2 input")
 	}
 
 	// Multiplied result of final horizontal position and final depth
 	mult := calculatePosition(inputFullTwo, inputWordsTwo, inputNumsTwo)
-	log.Infof("Multiplied position by depth: %v", mult)
+	log.Infof("Day 2 - Multiplied position by depth: %v", mult)
 }
